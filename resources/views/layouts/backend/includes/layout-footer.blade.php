7720379f9e17b1a7269abfce5f2e1263e157e8c1
Ahmad Dimyati <dimyati336@gmail.com>
2020-11-17 19:09:55 +0700

    menghapus logo 4vm di footer

--- resources/views/layouts/backend/includes/layout-footer.blade.php
+++ resources/views/layouts/backend/includes/layout-footer.blade.php
@@ -1,10 +1,11 @@
 <nav class="layout-footer footer bg-footer-theme">
     <div class="container-fluid d-flex flex-wrap justify-content-center justify-content-sm-between align-items-center container-p-x pb-3">
       <div class="pt-3">
-        Copyright © {{ now()->format('Y') }} <span class="footer-text font-weight-bolder">@lang('strip.copyright')<span>
+        {{-- Copyright © {{ now()->format('Y') }} <span class="footer-text font-weight-bolder">@lang('strip.copyright')<span> --}}
       </div>
       <div class="pt-3">
-        <span class="footer-text d-flex">Powered By <a href="https://www.4visionmedia.com" target="_blank" title="Perusahaan Jasa Pembuatan Website, Aplikasi, Digital Marketing, Design Company Profile, Software" style="display: inline-block; width: 100px; margin-left: 10px;"><img src="{{asset('assets/tmplts_backend/images/logo-4vm.svg')}}"></a></span>
+        {{-- <span class="footer-text d-flex">Powered By <a href="https://www.4visionmedia.com" target="_blank" title="Perusahaan Jasa Pembuatan Website, Aplikasi, Digital Marketing, Design Company Profile, Software" style="display: inline-block; width: 100px; margin-left: 10px;"><img src="{{asset('assets/tmplts_backend/images/logo-4vm.svg')}}"></a></span> --}}
+        Copyright © {{ now()->format('Y') }} <span class="footer-text font-weight-bolder">@lang('strip.copyright')<span>
       </div>
       <!-- <div>
         {{-- <a href="javascript:void(0)" class="footer-link pt-3">About Us</a>
