3ef799825529826ed3836b9e985b49ef65b7335e
Ahmad Dimyati <dimyati336@gmail.com>
2021-01-07 10:39:28 +0700

    fix bug data grades seeder

--- database/seeds/GradesSeeder.php
+++ database/seeds/GradesSeeder.php
@@ -20,58 +20,58 @@ class GradesSeeder extends Seeder
 
         $nilai = [
             0 => [
-                'minimum' => 100,
-                'maksimum' => 93,
+                'maksimum' => 100,
+                'minimum' => 93,
                 'keterangan' => 'A',
             ],
             1 => [
-                'minimum' => '92.99',
-                'maksimum' => 90,
+                'maksimum' => '92.99',
+                'minimum' => 90,
                 'keterangan' => 'A-',
             ],
             2 => [
-                'minimum' => '89.99',
-                'maksimum' => 87,
+                'maksimum' => '89.99',
+                'minimum' => 87,
                 'keterangan' => 'B+',
             ],
             3 => [
-                'minimum' => '86.99',
-                'maksimum' => 83,
+                'maksimum' => '86.99',
+                'minimum' => 83,
                 'keterangan' => 'B',
             ],
             4 => [
-                'minimum' => '82.99',
-                'maksimum' => 80,
+                'maksimum' => '82.99',
+                'minimum' => 80,
                 'keterangan' => 'B-',
             ],
             5 => [
-                'minimum' => '79.99',
-                'maksimum' => 77,
+                'maksimum' => '79.99',
+                'minimum' => 77,
                 'keterangan' => 'C+',
             ],
             6 => [
-                'minimum' => '76.99',
-                'maksimum' => 73,
+                'maksimum' => '76.99',
+                'minimum' => 73,
                 'keterangan' => 'C',
             ],
             7 => [
-                'minimum' => '72.99',
-                'maksimum' => 70,
+                'maksimum' => '72.99',
+                'minimum' => 70,
                 'keterangan' => 'C-',
             ],
             8 => [
-                'minimum' => '69.99',
-                'maksimum' => 67,
+                'maksimum' => '69.99',
+                'minimum' => 67,
                 'keterangan' => 'D+',
             ],
             9 => [
-                'minimum' => '66.99',
-                'maksimum' => 60,
+                'maksimum' => '66.99',
+                'minimum' => 60,
                 'keterangan' => 'D',
             ],
             10 => [
-                'minimum' => '59.99',
-                'maksimum' => 0,
+                'maksimum' => '59.99',
+                'minimum' => 0,
                 'keterangan' => 'F',
             ],
         ];
@@ -80,8 +80,8 @@ class GradesSeeder extends Seeder
             GradesNilai::create([
                 'kategori_id' => 1,
                 'creator_id' => 2,
-                'minimum' => $value['minimum'],
                 'maksimum' => $value['maksimum'],
+                'minimum' => $value['minimum'],
                 'keterangan' => $value['keterangan'],
             ]);
         }
