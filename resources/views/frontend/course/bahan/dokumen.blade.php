457afa81e8039b6ff2f7bb0e40a2f0ce3020e47f
wildanumarzent <wildanmuhamad961@gmail.com>
2021-08-25 16:41:35 +0700

    fix test

--- resources/views/frontend/course/bahan/dokumen.blade.php
+++ resources/views/frontend/course/bahan/dokumen.blade.php
@@ -2,10 +2,10 @@
 
 @section('content-view')
     @if ($data['bahan']->dokumen->bankData->file_type == 'pdf')
-    {{-- <iframe id="iframe" src="{{ route('bank.data.stream', ['path' => $data['bahan']->dokumen->bankData->file_path]) }}" frameborder="0" style="width:100%;height:7000px;">
-    </iframe> --}}
-    <iframe id="iframe" src="{{$data['pdf']}}" frameborder="0" style="width:100%;height:7000px;">
+    <iframe id="iframe" src="{{ route('bank.data.stream', ['path' => $data['bahan']->dokumen->bankData->file_path]) }}" frameborder="0" style="width:100%;height:7000px;">
     </iframe>
+    {{-- <iframe id="iframe" src="{{ route('bank.data.stream', $data['pdf']) }}" frameborder="0" style="width:100%;height:7000px;">
+    </iframe> --}}
     @elseif ($data['bahan']->dokumen->bankData->file_type == 'ppt' || $data['bahan']->dokumen->bankData->file_type == 'pptx')
     <iframe id="iframe" src="https://view.officeapps.live.com/op/view.aspx?src={{ route('bank.data.stream', ['path' => $data['bahan']->dokumen->bankData->file_path]) }}" frameborder="0" style="width:100%;height:7000px;">
     </iframe>
