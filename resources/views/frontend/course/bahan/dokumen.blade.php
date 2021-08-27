e91acf7b2ed48900d741478e9694120b5f210061
wildanumarzent <wildanmuhamad961@gmail.com>
2021-08-27 11:41:47 +0700

    fiix

--- resources/views/frontend/course/bahan/dokumen.blade.php
+++ resources/views/frontend/course/bahan/dokumen.blade.php
@@ -47,8 +47,9 @@
         padding: 1rem;
         }
         #pdf-render{
-        border: solid 1px blue;  
+        /* border: solid 1px blue;   */
         width: 100%;
+        height: 700px;
         }
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    
