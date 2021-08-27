9fc581da58384afe6fb5390a71c9b923548a9617
wildanumarzent <wildanmuhamad961@gmail.com>
2021-08-27 11:44:28 +0700

    fiix

--- resources/views/frontend/course/bahan/dokumen.blade.php
+++ resources/views/frontend/course/bahan/dokumen.blade.php
@@ -49,7 +49,7 @@
         #pdf-render{
         /* border: solid 1px blue;   */
         width: 100%;
-        height: 700px;
+        height: 100%;
         }
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    
