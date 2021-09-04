a7f765361173ad570eb648b305d09819f68b056f
wildanumarzent <wildanmuhamad961@gmail.com>
2021-09-04 23:39:41 +0700

    fix

--- resources/views/frontend/pelatihan/include/instruktor.blade.php
+++ resources/views/frontend/pelatihan/include/instruktor.blade.php
@@ -6,7 +6,7 @@
             {{-- <img src="{{ $data['mata']->creator->photo['filename'] != null ? asset('/userfile/photo/'.$data['mata']->creator->photo['filename'] != null) : asset('assets/img/5.png') }}" alt width="70px" class="ui-w-40 rounded-circle"> --}}
             <img src="{{ asset('/userfile/photo/'.$data['mata']->creator != null ? '/userfile/photo/'.$data['mata']->creator->photo['filename'] : '/userfile/photo/assets/img/5.png') }}" alt width="70px" class="ui-w-40 rounded-circle">
                 <div class="media-body pt-2 ml-3">
-                <h6 class="mb-2"> <strong style="color: grey">Teacher</strong></h6>
+                <h6 class="mb-2"> <strong style="color: grey">Instruktur</strong></h6>
                 <h6><strong style="color: rgb(53, 53, 53)">{{$data['mata']->creator->name}}</strong></h6>
             </div>
         </div>
